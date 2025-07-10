d634deed405208a5db4d89a150aa5cc141fd48ff
ogimenez-wirepas <ogimenez-wirepas@users.noreply.github.com>
2025-07-10 15:50:04 +0000

    deploy: 3ff284ac7ddbed0035fb5ae6aa689442c7ae300e

---
+++ v1.1.3/dc/d1b/rtc_8h.js
@@ -0,0 +1,24 @@
+var rtc_8h =
+[
+    [ "on_rtc_initialized", "dc/d1b/rtc_8h.html#a047a66ff21b6bdc0c937e8d61aff1bec", null ],
+    [ "rtc_timestamp_t", "dc/d1b/rtc_8h.html#ae772e2813a7313d20a870378f736e01e", null ],
+    [ "RTC_addInitializeCb", "dc/d1b/rtc_8h.html#a87617616fbb35e5a76af1895a2aab658", null ],
+    [ "RTC_getLocalTime", "dc/d1b/rtc_8h.html#ace00d2c6ba3ab4e14d276804df682490", null ],
+    [ "RTC_getTimezoneOffsetInSeconds", "dc/d1b/rtc_8h.html#ad8acb73066972c2bbb5266e8a7b1254d", null ],
+    [ "RTC_getUTCTime", "dc/d1b/rtc_8h.html#af6b422a85eda1c1e60abe93cf22fbc25", null ],
+    [ "RTC_init", "dc/d1b/rtc_8h.html#acb61e83ed1b4a31526e6c4cee5ef64b9", null ],
+    [ "RTC_removeInitializedCb", "dc/d1b/rtc_8h.html#a8a424c2a39d6781e2e6231d5a6f11a04", null ],
+    [ "provisioning_data_ids_e", "dc/d1b/rtc_8h.html#aee542072e8ed4c6421604a90538f9dc2", [
+      [ "RTC_ID_TIMESTAMP", "dc/d1b/rtc_8h.html#aee542072e8ed4c6421604a90538f9dc2a48d8e55630c25eacd43229f49680ebf1", null ],
+      [ "RTC_ID_TIMEZONE_OFFSET", "dc/d1b/rtc_8h.html#aee542072e8ed4c6421604a90538f9dc2a4a3f2add8548db9b69ec808237fee449", null ]
+    ] ],
+    [ "rtc_res_e", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0", [
+      [ "RTC_RES_OK", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0a6c56d7f04f0ce115f3a1c41bba60e22c", null ],
+      [ "RTC_UNAVAILABLE_YET", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0a3273f120adef0fac5ed6fd4f4def7406", null ],
+      [ "RTC_NO_MORE_CALLBACKS", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0a6e35ba450532611fb7e1b0deb294e6d8", null ],
+      [ "RTC_UNKNOWN_CALLBACK", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0a512c27722b5ebc7d779e553bf4708b4f", null ],
+      [ "RTC_INVALID_VALUE", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0aa3dacd74903cf7ca20649ab9c57e418c", null ],
+      [ "RTC_UNINITIALIZED", "dc/d1b/rtc_8h.html#af2379ec66412737cf38c395557f666f0a9b28fcfd63ae1c225321128fa0b8bdeb", null ]
+    ] ],
+    [ "RTC_VERSION", "dc/d1b/rtc_8h.html#a49466ee1ee0361d2f740264904b180d0", null ]
+];
---
+++ v1.1.3/dir_583822d7efe08ad8bbb58a435b38babe.js
@@ -0,0 +1,4 @@
+var dir_583822d7efe08ad8bbb58a435b38babe =
+[
+    [ "external_flash_gd25lb256e.h", "d3/dde/external__flash__gd25lb256e_8h.html", "d3/dde/external__flash__gd25lb256e_8h" ]
+];
